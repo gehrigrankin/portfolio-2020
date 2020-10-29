2dcd18e2cfad9cd2c09c9025ccfc05660af8b007
Gehrig <rgrankin22@gmail.com>
2020-10-29 00:36:43 -0700

    navbar icon compatible with bulma

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,27 +2,35 @@ import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAddressCard, faFileCode, faChartBar, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import Icon from 'react-bulma-components/lib/components/icon';
 
 import './Navbar.scss';
+
 
 export class NavbarContainer extends React.Component {
     render() {
         return (
             <div className="Navbar">
-                <div className="icon-tabs">
+                <div className="icon-container">
                     <div onClick={() => this.props.setActiveTab(1)} className={this.props.activeTab === 1 ? 'about-me active' : 'about-me'}>
-                        <FontAwesomeIcon size="3x" icon={faAddressCard} />
+                        <Icon>
+                            <FontAwesomeIcon size="2x" icon={faAddressCard} />
+                        </Icon>
                     </div>
-                    <div onClick={() => {
-                        console.log('here');
-                        this.props.setActiveTab(2)}} className={this.props.activeTab === 2 ? 'projects active' : 'projects'}>
-                        <FontAwesomeIcon size="3x" icon={faFileCode} />
+                    <div onClick={() => this.props.setActiveTab(2)} className={this.props.activeTab === 2 ? 'projects active' : 'projects'}>
+                       <Icon>
+                            <FontAwesomeIcon size="2x" icon={faFileCode} />
+                        </Icon>
                     </div>
                     <div onClick={() => this.props.setActiveTab(3)} className={this.props.activeTab === 3 ? 'skills active' : 'skills'}>
-                        <FontAwesomeIcon size="3x" icon={faChartBar} />
+                        <Icon>
+                            <FontAwesomeIcon size="2x" icon={faChartBar} />
+                        </Icon>
                     </div>
                     <div onClick={() => this.props.setActiveTab(4)} className={this.props.activeTab === 4 ? 'contact active' : 'contact'}>
-                        <FontAwesomeIcon size="3x" icon={faEnvelope} />
+                        <Icon>
+                            <FontAwesomeIcon size="2x" icon={faEnvelope} />
+                        </Icon>
                     </div>
                 </div>
             </div>
