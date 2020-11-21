3724ca184fe5bdaa35ca650c16e21bee3e4052ea
Gehrig <rgrankin22@gmail.com>
2020-11-21 14:57:46 -0700

    finished about me page

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -9,15 +9,19 @@ export const AboutMe = () => {
                 <p className="header-1">Gehrig Rankin</p>
                 <p className="header-2">Full Stack Web Developer</p>
             </div>
-            <div className="body column">
-                <p>To whom it may concern,
+            <div className="profile-pic">
+                
+            </div>
+            <div className="text">
+                To whom it may concern, <br/><br/>
 
                 Full stack web developer specializing in MERN stack development. Creating an elegant user experience and user interface is my passion, yet I still enjoy the connection of the back end and front end. For the past 2 years, I have been working as a full stack developer focusing on the front end side of the application. As a developer, I find my strengths are in my eye for design and the flow of a website.
-
+                <br/>
                 I have a deep passion for coding and enjoy it every single day. I love completing websites, as well as completing other developement projects of my own. I am a hard worker, overachiever, and a reliable source for any help you may need. I may not have the work experience that other coders possess but I assure you that I am motivated and hungry to create some outstanding web pages, I would love to show you what I can do!
 
-                Thank you for your time,
-                Gehrig Rankin</p>
+                <br/><br/>
+                Thank you for your time,<br/>
+                Gehrig Rankin
             </div>
         </div>
     );
