90a2fb8b141bcca4b0c9004925e86347418e012b
Gehrig <rgrankin22@gmail.com>
2020-11-22 01:28:32 -0700

    added skills page

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Skills.scss';
+
+export const Skills = () => {
+    return (
+        <div className="Skills page">
+            <div className="page-header font-bungee">
+                <p className="header-1">Skills</p>
+                <p className="header-2">Full Stack Web Developer</p>
+            </div>
+        </div>
+    );
+}
