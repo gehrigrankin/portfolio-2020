c5a506a89e9ec0bb2eff2d7ae4ee508eca559026
Gehrig <rgrankin22@gmail.com>
2020-11-23 03:51:11 -0700

    contact css mostly done + extended stars

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -1,56 +1,64 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faAddressBook } from '@fortawesome/free-solid-svg-icons';
+import Icon from 'react-bulma-components/lib/components/icon';
+
 import './ContactMe.scss';
 
 export const ContactMe = () => {
     return (
         <div className="ContactMe page">
-            <div className="page-header font-bungee">
+            <div className="page-header">
                 <p className="header-1">Contact Me</p>
+            </div>
+            <div className="container">
+                <div className="information">
+                    <p className="header-2">Email: </p>
+                    <p>Rgrankin22@gmail.com</p>
 
-                <div className="container">
-                    <div className="information">
-                        <p>Email: </p>
-                        <p>Rgrankin22@gmail.com</p>
+                    <p className="header-2">Phone: </p>
+                    <p>(703) 939 - 7355</p>
 
-                        <p>Phone: </p>
-                        <p>(703) 939 - 7355</p>
+                    <p className="header-2">LinkedIn: </p>
+                    <a target="_blank" href="https://www.linkedin.com/in/gehrigrankin/">linkedin.com/in/gehrigrankin</a>
 
-                        <p>Email: </p>
-                        <p>Rgrankin22@gmail.com</p>
+                    <p className="header-2">Github: </p>
+                    <a target="_blank" href="https://github.com/gehrigrankin">github.com/gehrigrankin</a>
+                </div>
+                <div className="email-me">
+                    <form action="mailto:rgrankin22@gmail.com" method="POST" encType="text/plain">
+                        <label class="label">Name</label>
+                        <div class="input-container">
+                            <Icon>
+                                <FontAwesomeIcon icon={faUser} />
+                            </Icon>
+                            <input type="text" placeholder="Jane Doe" />
+                        </div>
 
-                        <p>LinkedIn: </p>
-                        <a target="_blank" href="https://www.linkedin.com/in/gehrigrankin/">linkedin.com/in/gehrigrankin</a>
+                        <label class="label">Company</label>
+                        <div class="input-container">
+                            <Icon>
+                                <FontAwesomeIcon icon={faAddressBook} />
+                            </Icon>
+                            <input type="text" placeholder="ex. Trilogy Education" />
+                        </div>
 
-                        <p>LinkedIn: </p>
-                        <a target="_blank" href="https://github.com/gehrigrankin">github.com/gehrigrankin</a>
-                    </div>
-                    <div className="email-me">
-                        <form action="mailto:rgrankin22@gmail.com" method="POST" enctype="text/plain">
-                            {/* <label class="label">Name</label>
-                            <div class="control has-icons-left">
-                                <input class="input" type="text" placeholder="Jane Doe" />
-                                <span class="icon is-small is-left">
-                                    <i class="fas fa-user"></i>
-                                </span>
-                            </div>
+                        <label class="label">Message</label>
+                        <div className="input-container">
+                            <textarea class="textarea" placeholder="Textarea"></textarea>
+                        </div>
 
-                            <label class="label">Company</label>
-                            <div class="control has-icons-left">
-                                <input class="input" placeholder="ex. Trilogy Education" />
-                                <span class="icon is-small is-left">
-                                    <i class="fas fa-address-book"></i>
-                                </span>
-                            </div>
+                        {/* 
+                            
 
-                            <label class="label">Message</label>
-                            <div class="control">
-                                <textarea class="textarea" placeholder="Textarea"></textarea>
-                            </div>
+                            
+
+                            
+                            
 
                             <button class="button is-link has-background-primary" type="submit">Submit</button> */}
-                        </form>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
