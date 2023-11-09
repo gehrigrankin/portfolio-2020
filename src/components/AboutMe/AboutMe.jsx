f1b5141164bab1b042658c9bb5d499e01f929ed2
Gehrig Rankin <rgrankin22@gmail.com>
2023-11-08 17:14:16 -0700

    fixed up about me page

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container, Row, Col, Image } from 'react-bootstrap';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedinIn, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 // import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 import gehrigHeadshot from './gehrigrankin.jpg'
@@ -24,41 +24,22 @@ export const AboutMe = () => {
                     </Row>
                     <Row>
                         <div className="information w-100">
-                            <p className="text-center">Rgrankin22@gmail.com</p>
+                            <p className="text-center pt-4">Rgrankin22@gmail.com</p>
                             <p className="text-center">(703) 939-7355</p>
 
-
-
-                            <Row className="info-icons pt-4 mx-3">
+                            <Row className="info-icons pt-4 mx-3 w-100">
                                 <Col sm={6} className="text-center">
-                                    <FontAwesomeIcon className="linkedin-icon" icon={faLinkedinIn} />
+                                    <a target="_blank" href="https://www.linkedin.com/in/gehrigrankin"><FontAwesomeIcon className="linkedin-icon" icon={faLinkedin} /></a>
                                 </Col>
-                                <Col sm={6} className="text-center">
-                                    <FontAwesomeIcon className="github-icon" icon={faGithub} />
+                                <Col sm={6} className="text-center d-flex">
+                                    <a target="_blank" href="https://www.github.com/gehrigrankin?tab=repositories"><FontAwesomeIcon className="github-icon" icon={faGithub} /></a>
                                 </Col>
                             </Row>
-
-                            
-
-
-
-
-                            
-
-
-
-                            {/* <p className="header-2">LinkedIn: </p>
-                            <a target="_blank" href="https://www.linkedin.com/in/gehrigrankin/">linkedin.com/in/gehrigrankin</a>
-
-                            <p className="header-2">Github: </p>
-                            <a target="_blank" href="https://github.com/gehrigrankin">github.com/gehrigrankin</a> */}
                         </div>
                     </Row>
                 </Col>
                 <Col xs="" className="text-card">
                     <div className="text">
-                        To whom it may concern, <br /><br />
-
                         Full stack web developer specializing in Javascript and modern front end libraries. For the past 6 years I've been in a couple of full stack roles but ended up doing most of my contributions on the front end. As a developer I find my strengths are in my eye for page layout design and my ability to create user experiences that are simple yet effective at the same time.
                         <br />
                         <br />
