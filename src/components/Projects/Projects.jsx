f54e658361753880288c883464fb636574043c27
Gehrig Rankin <rgrankin22@gmail.com>
2023-11-11 00:24:12 -0700

    added messenger project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useState } from 'react';
-import { Container, Row, Col, Carousel, Image } from 'react-bootstrap';
-import Project from './components/Project'
+import { Row, Col, Carousel, Image } from 'react-bootstrap';
 
+import messengerImg from './images/messenger-pic.PNG';
 import azcImg from './images/azcImage.png';
 import skellyImg from './images/Skelly.png';
 import groupGenImg from './images/Group-gen.png';
@@ -29,29 +29,36 @@ export const Projects = () => {
                     <Carousel.Item interval={99999999} className="h-100">
                         <Image
                             className="d-block m-auto"
+                            src={messengerImg}
+                            alt="Real Time Messenger screenshot"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item interval={99999999} className="h-100">
+                        <Image
+                            className="d-block m-auto"
                             src={azcImg}
-                            alt="First slide"
+                            alt="AZ Central Scraper screenshot"
                         />
                     </Carousel.Item>
                     <Carousel.Item interval={99999999} className="h-100">
                         <Image
                             className="d-block m-auto"
                             src={iionsLogo}
-                            alt="Second slide"
+                            alt="IIONS screenshot"
                         />
                     </Carousel.Item>
                     <Carousel.Item interval={99999999} className="h-100">
                         <Image
                             className="d-block m-auto"
                             src={skellyImg}
-                            alt="Third slide"
+                            alt="Skelly screenshot"
                         />
                     </Carousel.Item>
                     <Carousel.Item interval={99999999} className="h-100">
                         <Image
                             className="d-block m-auto"
                             src={groupGenImg}
-                            alt="Fourth slide"
+                            alt="Group Generator screenshot"
                         />
                     </Carousel.Item>
                 </Carousel>
@@ -64,61 +71,21 @@ export const Projects = () => {
                         <p className="header-3 monospace mb-3">{ProjectsArr[index].techUsed}</p>
 
                         {ProjectsArr[index].githubLink ? 
-                            <span className="monospace grey-font mr-5">
+                            <p className="monospace grey-font mr-5">
                                 Link to codebase:  <a target="_blank" href={`https://${ProjectsArr[index].githubLink}`}  className="secondary-red-font">{ProjectsArr[index].githubLink}</a>
-                            </span>
+                            </p>
                         : null}
 
                         {ProjectsArr[index].webpageLink ? 
-                            <span className="monospace grey-font ml-5">
+                            <p className="monospace grey-font">
                                 Deployed Webpage: <a href={ProjectsArr[index].webpageLink} target="_blank" className="secondary-red-font">{ProjectsArr[index].webpageLink}</a>
-                            </span>
+                            </p>
                             : null}
-                        
-                       
-                        
 
                         <p className="mt-4">{ProjectsArr[index].description}</p>
                     </div>
                     </Col>
                 </Row>
-                
-                {/* <Row>
-                    <Col className="mt-3" md={12} lg={6} xl={4}>
-                        <Project
-                            title="AZ Central Scraper"
-                            description={descriptions.azc}
-                            src={azcImg}
-                            btnTxt="Launch"
-                            btnSrc="https://still-brook-83736.herokuapp.com/"
-                        />
-                    </Col>
-                    <Col className="mt-3" md={12} lg={6} xl={4}>
-                        <Project
-                            title="IIONS"
-                            description={descriptions.iions}
-                            src={iionsLogo}
-                        />
-                    </Col>
-                    <Col className="mt-3" md={12} lg={6} xl={4}>
-                        <Project
-                            title="Skelly"
-                            description={descriptions.skelly}
-                            src={skellyImg}
-                            btnTxt="Launch"
-                            btnSrc="https://github.com/ivme83/skeletor"
-                        />
-                    </Col>
-                    <Col className="mt-3" md={12} lg={6} xl={4}>
-                        <Project
-                            title="Group Generation"
-                            description={descriptions.groupGen}
-                            src={groupGenImg}
-                            btnTxt="Launch"
-                            btnSrc="https://still-brook-83736.herokuapp.com/"
-                        />
-                    </Col>
-                </Row> */}
             </div>
         </div>
     );
